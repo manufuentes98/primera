74128e17acca056c5369123b43b3181fcb0e8c29
manufuentes98 <36626964+manufuentes98@users.noreply.github.com>
2018-02-26 14:02:39 +0100

    amo github versiculos 15-20

--- src/primera.java
+++ src/primera.java
@@ -1,6 +1,6 @@
 
 public class primera {
 	int unAtributo;
-	int atributoDos;
-
+	int atributoDos=4;
+	
 }
--- src/segunda.java
+++ src/segunda.java
@@ -1,4 +1,12 @@
 
 public class segunda {
 int dos;
+
+
+public segunda(int dos) {
+	super();
+	this.dos = dos;
+	primera suicidio= new primera();
+	suicidio.atributoDos=5;
+}
 }
